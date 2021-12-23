0f028ceb570e54afb3e369a61e7265e61082d8da
unknown <nikonova@hotmail.de>
2021-12-23 17:29:08 +0100

    fetching data from Database works

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,4 +1,5 @@
 
+/* its a seperate script that we can run to import data. we need to Import all this cause its not connected to our server. its seperatly */ 
 
 
 import mongoose from 'mongoose'
@@ -17,18 +18,24 @@ connectDB()
 
 const importData = async () => {
   try {
+
+ 
     await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
 
+
+    // with insertMany we passing our useres Object into the dataBase
+    //here we just want to store the first object of our array. 
     const createdUsers = await User.insertMany(users)
 
     const adminUser = createdUsers[0]._id
 
+    //for the products we creating a var to store our manipulated data 
     const sampleProducts = products.map((product) => {
       return { ...product, user: adminUser }
     })
-
+    // here we passing it to our Database 
     await Product.insertMany(sampleProducts)
 
     console.log('Data Imported!'.green.inverse)
@@ -58,3 +65,7 @@ if (process.argv[2] === '-d') {
 } else {
   importData()
 }
+
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
-/*  withoug Es Module 
+/*  without ES modules
 const express = require('express')
 const dotenv = require('dotenv')
 const products = require('./data/products')
-
-
-Hier holen wir uns die Daten vom BackendServer (grade von unserer DummyData from Importieren )
 */
+
+/* connectinon to DB and linking to file where we fetch data from MongoDB */ 
 
 import express from 'express'
 import dotenv from 'dotenv'
-import products  from './data/products.js'
+//import products  from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
+
+import productRoutes from './routes/productRoutes.js'
 
 
 dotenv.config();
@@ -25,18 +26,26 @@ app.get('/', (req,res)=>{
    res.send('API is running')
 })
 
-//creating a route to display the product array under this adress
-app.get('/api/products', (req,res)=>{
-  //u can use res.send both will converted the  product array to json data //respont with the  Products. js Array 
-   res.json(products)
-})
+// we mount it here: 
+//antything that goes to /api/products is going to be 
+//linked to productRoutes. 
+app.use('/api/products', productRoutes )
 
-//creating a route to display a single product 
-app.get('/api/products/:id', (req,res)=>{
-  //u can  get the /:id through req object 
-  const product = products.find(p => p._id === req.params.id)
-   res.json(product)
-})
+/* moved to productRoutes './routes/productRoutes.js'
+//creating a route to display the product array under this adress
+router.get('/api/products', (req,res)=>{
+   //u can use res.send both will converted the  product array to json data //respont with the  Products. js Array 
+    res.json(products)
+ })
+ 
+ //creating a route to display a single product 
+ router.get('/api/products/:id', (req,res)=>{
+   //u can  get the /:id through req object 
+   const product = products.find(p => p._id === req.params.id)
+    res.json(product)
+ })
+ 
+*/ 
 
 const PORT = process.env.PORT  || 5000
 
